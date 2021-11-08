75fe8207c233473c867dfc97fb7f7cbee3808606
YudaDwi <yudadwi0@gmail.com>
2021-11-08 09:31:17 +0700

    3 add theme

---
+++ airdrop/lib/shared/theme.dart
@@ -0,0 +1,26 @@
+part of 'shared.dart';
+
+//ox itu menunjukan bahwa kita menggunakan hexcode, ff memberitahu opacity warna tsb 100%
+Color kPrimaryColor = Color(0xff5C40CC);
+Color kBlackColor = Color(0xff1F1449);
+Color kWhiteColor = Colors.white;
+Color kGreyColor = Color(0xff9698A9);
+Color kGreenColor = Color(0xff0EC3AE);
+Color kRedColor = Color(0xffEB70A5);
+Color kBackgroundColor = Color(0xffFAFAFA);
+Color kInActiveColor = Color(0xffFAFAFA);
+
+TextStyle blacktTextSyle = GoogleFonts.poppins(color: kBlackColor);
+TextStyle whiteTextSyle = GoogleFonts.poppins(color: kWhiteColor);
+TextStyle greyTextSyle = GoogleFonts.poppins(color: kGreyColor);
+TextStyle redTextSyle = GoogleFonts.poppins(color: kRedColor);
+TextStyle greenTextSyle = GoogleFonts.poppins(color: kGreenColor);
+TextStyle purpleTextSyle = GoogleFonts.poppins(color: kPrimaryColor);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extraBold = FontWeight.w800;
+FontWeight black = FontWeight.w900;

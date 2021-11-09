f040259fd71aa096b64e377b8c48d540c6d01bf7
YudaDwi <yudadwi0@gmail.com>
2021-11-09 09:18:56 +0700

    10 slicing signup input section1

--- airdrop/lib/ui/pages/sign_up_page.dart
+++ airdrop/lib/ui/pages/sign_up_page.dart
@@ -15,12 +15,54 @@ class SignUpPage extends StatelessWidget {
       );
     }
 
+    Widget inputSection() {
+      Widget emailInput() {
+        return Container(
+          margin: EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Full Name',
+                style: blacktTextSyle,
+              ),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                    hintText: 'Your Full Name',
+                    hintStyle: greyTextSyle.copyWith(fontSize: 16),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(defaultRadius)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(defaultRadius),
+                        borderSide: BorderSide(color: kPrimaryColor))),
+              )
+            ],
+          ),
+        );
+      }
+
+      return Container(
+        margin: EdgeInsets.only(top: 30),
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+        decoration: BoxDecoration(
+            color: kWhiteColor,
+            borderRadius: BorderRadius.circular(defaultRadius)),
+        child: Column(
+          children: [emailInput()],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: SafeArea(
         child: ListView(
           padding: EdgeInsets.symmetric(horizontal: defaultMargin),
-          children: [title()],
+          children: [title(), inputSection()],
         ),
       ),
     );
